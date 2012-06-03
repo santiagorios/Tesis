58a78783fedf6207d2b3379643be84bb4bc0465c
Santiago Rios <santiago@x-team.com>
2012-06-02 23:42:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtusKnowledge::Application.config.secret_token = 'c13269c246be149bd8808400251f65f6f8af430ebb39c7f0e5264a0f2c3f978be1bfd4cff627464b80dc8de893ccaa35e2f70737a0b710084c7fce397e086810'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+VirtusKnowledge::Application.config.session_store :cookie_store, key: '_virtus-knowledge_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# VirtusKnowledge::Application.config.session_store :active_record_store
