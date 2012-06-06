c67796764b533bb7a169a7f43278fa36c925be73
Santiago Rios <santiago@x-team.com>
2012-06-05 20:29:46 -0500

    Adds validation for the tile and the default values when a entry is going to be created

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,10 @@
 class Entry < ActiveRecord::Base
+	validates :title, :presence => true
+
+	after_initialize :default_values
+	
+	private
+		def default_values
+			self.date ||= Time.now.strftime("%Y-%m-%d")
+		end
 end
