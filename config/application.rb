5d643392850304a4f7f1a4cd2f949081e3eedd80
Santiago Rios <santiago@x-team.com>
2012-06-09 14:00:30 -0500

    Adds sanitation to the outputs of the entries, it also adds the basic config to the application.rb file

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module VirtusKnowledge
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_view.sanitized_allowed_attributes = 'id', 'class', 'style'
   end
 end
