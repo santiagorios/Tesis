00b9f21752d15a0f6205fd783e5b9bc93da4aa98
Santiago Rios <santiago@x-team.com>
2012-06-10 10:56:42 -0500

    Adds the requirement of being logged in to add, edit, update or delete entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,5 @@
 class EntriesController < ApplicationController
+  before_filter :require_login, :only => [:new, :edit, :create, :update, :destroy]
   # GET /entries
   # GET /entries.json
   def index
