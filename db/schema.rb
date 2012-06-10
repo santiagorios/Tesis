7bf62c1368022a5b8d0c9ff327497bac85e6b1ef
Santiago Rios <santiago@x-team.com>
2012-06-09 20:33:33 -0500

    Adds the migration and the makes a small change in the sorcery initializer for the brute force functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120610010219) do
+ActiveRecord::Schema.define(:version => 20120610012730) do
 
   create_table "entries", :force => true do |t|
     t.string   "title"
@@ -23,13 +23,16 @@ ActiveRecord::Schema.define(:version => 20120610010219) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                        :null => false
+    t.string   "email",                                       :null => false
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.integer  "failed_logins_count",          :default => 0
+    t.datetime "lock_expires_at"
+    t.string   "unlock_token"
   end
 
   add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
