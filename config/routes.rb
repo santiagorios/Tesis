f474aaff198839957a17a009a187559ff3f07656
Santiago Rios <santiago@x-team.com>
2012-06-09 19:54:27 -0500

    Adds the logic for user management and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 VirtusKnowledge::Application.routes.draw do
-  get "sessions/new"
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
 
   get "signup" => "users#new", :as => "signup"
 
