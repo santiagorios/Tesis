9d3c43507ecb29901a1a58223594cfa329b2837f
Santiago Rios <santiago@x-team.com>
2012-06-10 14:46:15 -0500

    Adds the edit profile functionality to the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,11 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_format_of :email, :with => /^[^@][\w.-]+@espe.edu.ec/i
 
-  validates_length_of :password, :minimum => 6
+  validates_length_of :password, :minimum => 6, :on => :create
+  validates_length_of :password, :minimum => 6, :on => :update, :allow_blank => true
 
 
   has_one :profile
+  accepts_nested_attributes_for :profile
+  attr_accessible :profile_attributes
 end
