15d0c2105b3a773ea8582c0bbdd68c6c2e3e5b8b
Santiago Rios <santiago@x-team.com>
2012-06-12 21:43:35 -0500

    Updates the go back button functionality

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,6 +1,6 @@
 module EntriesHelper
-  def just_created
-    if URI(request.referer).path == '/entries/new'
+  def just_created_or_updated
+    if URI(request.referer).path == '/entries/new' || URI(request.referer).path.match(/\/entries\/[0-9]+\/edit/i)
       true
     else
       false
