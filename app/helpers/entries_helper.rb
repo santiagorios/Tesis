4a3ecf0f57155e6f8de862e3930121516fb3e775
Santiago Rios <santiago@x-team.com>
2012-06-16 18:59:37 -0500

    Fixes the problem when a user access directly to a entry show page

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,9 +1,21 @@
 module EntriesHelper
   def just_created_or_updated
-    if URI(request.referer).path == '/entries/new' || URI(request.referer).path.match(/\/entries\/[0-9]+\/edit/i)
-      true
-    else
-      false
+  	if uri?(request.referer)
+	    if URI(request.referer).path == '/entries/new' || URI(request.referer).path.match(/\/entries\/[0-9]+\/edit/i)
+	      true
+	    else
+	      false
+	    end
     end
   end
+
+  def uri?(string)
+    uri = URI.parse(string)
+    %w( http https ).include?(uri.scheme)
+  rescue URI::BadURIError
+    false
+  rescue URI::InvalidURIError
+    false
+  end
+
 end
