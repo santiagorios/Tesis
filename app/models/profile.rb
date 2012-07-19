3912da7ec4a0b34db5c2403bdfdec2bacc261337
Santiago Rios <santiago@x-team.com>
2012-07-18 21:03:20 -0500

    Adds the image to the user form

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ActiveRecord::Base
+	attr_accessible :user_id, :name, :birthday, :phone, :about_me, :image, :remote_image_url
 	belongs_to :user
+	mount_uploader :image, ImageUploader
 end
