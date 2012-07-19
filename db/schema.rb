654156fffbff41cca4b3e956ac4b20ee3e3734a9
Santiago Rios <santiago@x-team.com>
2012-07-18 21:01:27 -0500

    Adds the column image to the profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120710032245) do
+ActiveRecord::Schema.define(:version => 20120717031955) do
 
   create_table "departments", :force => true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20120710032245) do
     t.text     "about_me"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "research_lines", :force => true do |t|
