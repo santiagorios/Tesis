4d5d3a86a393418103f11447b0213efca737c44b
Santiago Rios <santiago.rios@vyrtus.com>
2012-09-17 21:27:55 -0500

    Temporary fix for homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def home
     @entries_carousel = Entry.order("random()").where(:published => true).joins(:images).all(:limit => 3, :group => :id)
-    @users = User.order("random()").joins(:entries).all(:limit => 3, :group => :id)
+    #@users = User.order("random()").joins(:entries).all(:limit => 3, :group => :id)
     @departments = Department.order("random()").joins(:entries).all(:limit => 3, :group => :id)
+    if(@entries_carousel.empty? && @users.empty? && @departments.empty? )
+      respond_to do |format|
+        format.html { render :template => 'pages/empty' }
+      end
+    end
     #@entries_list =
   end
 
