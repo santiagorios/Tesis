37d0866a5f0d50c06af2b758e0eb1f14d97eda3c
Santiago Rios <santiago.rios@vyrtus.com>
2012-09-17 23:17:02 -0500

    Changes the random function to rand

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
-    @entries_carousel = Entry.order("random()").where(:published => true).joins(:images).all(:limit => 3, :group => :id)
-    @users = User.order("random()").joins(:entries).all(:limit => 3, :group => :id)
-    @departments = Department.order("random()").joins(:entries).all(:limit => 3, :group => :id)
+    @entries_carousel = Entry.order("rand()").where(:published => true).joins(:images).all(:limit => 3, :group => :id)
+    @users = User.order("rand()").joins(:entries).all(:limit => 3, :group => :id)
+    @departments = Department.order("rand()").joins(:entries).all(:limit => 3, :group => :id)
     if @entries_carousel.empty? && @users.empty? && @departments.empty?
       respond_to do |format|
         format.html { render :template => 'pages/empty' }
