123eee2f8995dbca57efa6c4877ed35d451d1aef
Santiago Rios <santiago.rios@vyrtus.com>
2012-09-25 11:56:20 -0500

    Fixes a problem in the explore page

--- config/navigations/user_navigation.rb
+++ config/navigations/user_navigation.rb
@@ -6,7 +6,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
   navigation.items do |primary|
     primary.item :users, t('application.sidebar_title_users'), users_path, :class => 'nav-sub-header' do |sub_nav|
-      users = User.order('random()').joins(:profile).where('profiles.name IS NOT NULL').all(:limit => 5, :group => :email)
+      users = User.order('rand()').joins(:profile).where('profiles.name IS NOT NULL').all(:limit => 5, :group => :email)
       if !users.empty?
         for user in users do
           sub_nav.item :user, user.profile.try(:name), url_for(user)
