18fac191022f9beb84df8af4ca0b15fc68407ff2
Santiago Rios <santiago.rios@vyrtus.com>
2013-02-05 23:43:53 -0500

    Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
@@ -5,29 +6,76 @@ ActiveAdmin.register_page "Dashboard" do
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
       span :class => "blank_slate" do
-        span "Welcome to Active Admin. This is the default dashboard page."
-        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
+        small "Bienvenido a la Administración" 
+        span "Sistema de Difusión de la gestión del conocimiento de la ESPE."
+        
+      end
+    end
+    div :class => "blank_slate_container", :id => "dashboard_default_message" do
+        br
+    end
+    # Here is an example of a simple dashboard with columns and panels.
+    #
+
+    columns do
+      column do
+        panel "Roles" do
+          ul do
+            Role.order("rand()").all(:limit => 5).map do |department|
+              li link_to(department.name, admin_department_path(department))
+            end
+          end
+        end
+      end
+      column do
+        panel "Líneas de Investigación" do
+          ul do
+            ResearchLine.order("rand()").all(:limit => 5).map do |department|
+              li link_to(department.name, admin_department_path(department))
+            end
+          end
+        end
+      end
+      column do
+        panel "Sub-líneas de Investigación" do
+          ul do
+            ResearchSubline.order("rand()").all(:limit => 5).map do |department|
+              li link_to(department.name, admin_department_path(department))
+            end
+          end
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Usuarios" do
+          link_to("Usuarios", admin_users_path)
+        end
+      end
+      
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Departamentos" do
+          ul do
+            Department.order("rand()").all(:limit => 5).map do |department|
+              li link_to(department.name, admin_department_path(department))
+            end
+          end
+        end
+      end
+    #end
+
+    #columns do
+      column do
+        panel "Areas de Conocimiento" do
+          ul do
+            KnowledgeArea.order("rand()").all(:limit => 5).map do |department|
+              li link_to(department.name, admin_department_path(department))
+            end
+          end
+        end
+      end
+    end
   end # content
 end
